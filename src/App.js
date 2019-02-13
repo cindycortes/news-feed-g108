b8862ffaae096d86f670d52e87a1c6706a191bd7
Cindy Cortes <cindy.indins@gmail.com>
2019-02-13 14:24:23 -0700

    used the fetch method to grab the data in console

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    articles: [],
+    // url: "https://localhost:3001/articles"
+
+  }
+  
+  async componentDidMount() {
+    const response = await fetch(`http://localhost:3001/articles`)
+    const articles = await response.json();
+    this.setState({ articles });
+  }catch (error) {
+    console.log(error)
+  }
+  
+
   render() {
+    console.log(this.state.articles);
+
     return (
       <div>Article App</div>
     );  
