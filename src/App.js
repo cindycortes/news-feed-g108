5b4a6364f0aa335bd4791c7e272a166944bd7c7f
Cindy Cortes <cindy.indins@gmail.com>
2019-02-15 09:07:26 -0700

    Just need submi functionality to work

--- src/App.js
+++ src/App.js
@@ -2,50 +2,72 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import './App.css';
 import ArticleList from './components/ArticleList';
-import AddArticle from './components/AddArticle';
+// import AddArticle from './components/AddArticle';
 import Footer from './components/Footer';
+import Axios from 'axios';
 class App extends Component {
 
   state = {
-    articles: [
-    ]
-
+    articles: []
   }
 
-  componentDidMount = async () => {
-    try {
-      const res = await fetch(`http://localhost:3001/articles`);
-      const json = await res.json();
-      this.setState({
-        articles: json.map(article => {
+  // componentDidMount = async () => {
+  //   try {
+  //     const res = await fetch(`http://localhost:3001/articles`);
+  //     const json = await res.json();
+  //     this.setState({
+  //       articles: json.map(article => {
+  //         return {
+  //           ...article
+  //         }
+  //       })
+  //     })
+  //   } catch (e) {
+  //     alert(e);
+  //   }
+  // }
+
+  componentDidMount() {
+    Axios.get(`http://localhost:3001/articles`)
+      .then(response => this.setState({ articles: response.data }))
+  }
+
+  addArticle = newArticle => {
+    const { title, img } = newArticle;
+    fetch(`http://localhost:3001/articles`, {
+      method: "post",
+      body: JSON.stringify({ title, img }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState(prevState => {
           return {
-            ...article
+            article: [
+              ...prevState.article,
+              {
+                title,
+                img
+              }
+            ]
           }
         })
       })
-    }catch (e) {
-      alert(e);
-    }
   }
 
-  // addArticleToList = newArticle => {
-  //   this.setState(prevState => {
-  //     return {
-  //       AddedArticles : [...prevState.articles, newArticle]
-  //     }
-  //   })
-  // }
+
+
+
 
   render() {
-    // console.log(this.state.articles);
 
     return (
       <div className="container">
         <Header />
 
         <ArticleList articles={this.state.articles} />
-
-        {/* <AddArticle articles={this.state.articles} addArticleToList={this.addArticleToList} /> */}
 
         <Footer />
 
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 import Article from './Article'
 
 class ArticleList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            articleTitle: props.articleTitle,
-            articleImg: props.articleImg
-        }
+    state = {
+        title: "",
+        img: ""
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
-        const data = this.state
-        console.log(data)
+        const newArticle = {
+            
+            title: this.state.title,
+            img: this.state.img
+        }
+        this.props.addArticle(newArticle)
     }
 
     handleInputChange = (e) => {
@@ -30,32 +31,33 @@ class ArticleList extends Component {
         const { articleTitle, articleImg } = this.state
         const { articles } = this.props
 
-        console.log(articles);
-        const articleList = articles.map(article => {
+        // console.log(articles);
+        
+        let listOfArticles = articles.map(article => {
             return <Article key={article.id} article={article} />
         })
 
 
         return (
             <>
-                {articleList}
+                {listOfArticles}
 
-                <p>Article Title is : {articleTitle} </p>
-                <p>Article Img is : {articleImg} </p>
+                {/* <p>Article Title is : {articleTitle} </p>
+                <p>Article Img is : {articleImg} </p> */}
 
 
                 <div className="container">
 
                     <h1 className="article">Add An Article</h1>
-                    <form onSubmit={this.handleSubmit.bind(this)}>
+                    <form onSubmit={this.handleSubmit}>
                         <div className="formgroup">
                             <label>Article Title</label>
-                            <input type="text" name="articleTitle" className="form-control" onChange={this.handleInputChange} placeholder="Article Title" ></input>
+                            <input type="text" name="title" value={this.state.title} className="form-control" onChange={this.handleInputChange} placeholder="Article Title" ></input>
                         </div>
 
                         <div className="formgroup">
                             <label>Image</label>
-                            <input type="text" name="articleImg" className="form-control" onChange={this.handleInputChange} placeholder="Img Link..."></input>
+                            <input type="text" name="img" value={this.state.img}className="form-control" onChange={this.handleInputChange} placeholder="Img Link..."></input>
                         </div>
                         <button type="submit" className="btn btn-primary mb-2">Submit</button>
                     </form>
